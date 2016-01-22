8f50922d84f97333ee5f6506e3398ccd14b2a71c
Dominik <domin1204@gmail.com>
2016-01-22 17:28:59 +0100

    First version
    Added fundamential files with reset stylesheet, witch helps to prepare
    website for different browsers

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+//register menu
+add_action( 'after_setup_theme', 'register_primary_menu' );
+
+function register_primary_menu() {
+  register_nav_menu( 'primary', __( 'Primary', 'theme-slug' ) );
+}
+
+//register admin menu theme settings
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
+
+function register_my_custom_menu_page(){
+	add_menu_page( 'Theme menu', 'Theme menu', 'manage_options', 'theme_menu', 'my_custom_menu_page', 'dashicons-admin-home', 60 );
+}
+
+function my_custom_menu_page(){
+  include('admin/theme-menu.php');
+}
+
+//shortcode: [foobar]
+add_shortcode( 'foobar', 'foobar_func' );
+
+function foobar_func( $atts ){
+	return "foo and bar";
+}
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  </head>
+  <body>
+    <header>
+      <h1>HEADER</h1>
+      <nav>
+        <?php wp_nav_menu( array('menu' => 'Primary' )); ?>
+      </nav>
+    </header>
