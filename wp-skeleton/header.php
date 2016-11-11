6fd85a4725414eb659d96a49cdc966f0bcd1d6cf
Dominik Nowak <domin1204@gmail.com>
2016-11-11 12:10:46 +0100

    Added meta tags for RWD

--- wp-skeleton/header.php
+++ wp-skeleton/header.php
@@ -2,6 +2,8 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/reset.css">
