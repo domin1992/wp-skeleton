a6c23768400eab310c5272d171f74f2cbd3d1908
Dominik Nowak <domin1204@gmail.com>
2016-11-11 12:07:57 +0100

    Changed functions comment styling

--- wp-skeleton/functions.php
+++ wp-skeleton/functions.php
@@ -1,12 +1,18 @@
 <?php
-//register menu
+
+// =========================================== //
+// register menu                               //
+// =========================================== //
 add_action( 'after_setup_theme', 'register_primary_menu' );
 
 function register_primary_menu() {
   register_nav_menu( 'primary', __( 'Primary', 'theme-slug' ) );
 }
+// ------------------------------------------- //
 
-//register admin menu theme settings
+// =========================================== //
+// register admin menu theme settings          //
+// =========================================== //
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
 function register_my_custom_menu_page(){
@@ -16,10 +22,14 @@ function register_my_custom_menu_page(){
 function my_custom_menu_page(){
   include('admin/theme-menu.php');
 }
+// ------------------------------------------- //
 
-//shortcode: [foobar]
+// =========================================== //
+// shortcode: [foobar]                         //
+// =========================================== //
 add_shortcode( 'foobar', 'foobar_func' );
 
 function foobar_func( $atts ){
 	return "foo and bar";
 }
+// ------------------------------------------- //
