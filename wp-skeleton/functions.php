0aa10a0090e564d889a67ec9816f0d62bed1305a
Unknown <domin1204@gmail.com>
2018-07-11 15:05:52 +0200

    Added custom mime types

--- wp-skeleton/functions.php
+++ wp-skeleton/functions.php
@@ -171,3 +171,15 @@ function wp_skeleton_registerCustomPostType(){
 add_action('init', 'wp_skeleton_registerCustomPostType');
 */
 // ------------------------------------------- //
+
+// =========================================== //
+// Allow custom mime types                     //
+// =========================================== //
+/*
+function wp_skeleton_AllowMimeTypes($mimes){
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'wp_skeleton_AllowMimeTypes');
+*/
+// ------------------------------------------- //
